c6306e3707aab85829f23877287b8db7049654c5
Valeriy Zainullin <Autaren@gmail.com>
2021-02-19 22:39:33 +0300

    Изменил "IndentationChecker.c". - Заменил общий для всех плагинов код на вызовы функций из общего заголовочного файла. - Добавил проверку типов отступов соседних строк в последовательности тех, у которых есть отступы (начинающие строку пробельные символы). Определил функцию, вызываемую при перекраске экрана.# Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- src/IndentationChecker.c
+++ src/IndentationChecker.c
@@ -11,27 +11,49 @@ static const wchar_t* DESCRIPTION = L"Checks indentation. Colors tabs if both sp
 static const VersionInfo PLUGIN_VERSION = {1, 0, 0, 1, VS_RELEASE};
 
 static const unsigned int PLUGIN_COLORING_PRIORITY = 2;
-static const FarColor PLUGIN_COLOR_USED = {FCF_4BITMASK, {0}, {14}, {0, 0}};
+static const FarColor PLUGIN_SPACE_COLOR = {FCF_4BITMASK, {0}, {4}, {0, 0}};
+static const FarColor PLUGIN_TAB_COLOR = {FCF_4BITMASK, {0}, {14}, {0, 0}};
+
+enum TypeOfIndentation {
+	TABS_INDENTATION,
+	SPACES_INDENTATION,
+	MIXED_INDENTATION
+};
+typedef enum TypeOfIndentation TypeOfIndentation;
+
+bool isFirstLine;
+static TypeOfIndentation lastTypeOfIndentation;
+static RegisterValue lastLineWithIndentation;
+static void newColoring() {
+	isFirstLine = true;
+}
 
 static bool isSpaceCharacter(const wchar_t character) {
 	return character == L' ' || character == L'\t';
 }
 
+void colorLeadingSpaceCharacters(RegisterValue lineNumber) {
+	const wchar_t* line;
+	RegisterValue lineLength = 0;
+	getLine(lineNumber, &line, &lineLength);
+	assert(lineLength != 0);
+	EXPLICITLY_UNUSED(lineLength);
+
+	for (RegisterValue pos = 0; pos < lineNumber; ++pos) {
+		if (line[pos] == L' ') {
+			colorCharacter(lineNumber, pos, PLUGIN_SPACE_COLOR, ECF_NONE);
+		} else if (line[pos] == L'\t') {
+			colorCharacter(lineNumber, pos, PLUGIN_TAB_COLOR, ECF_NONE);
+		} else {
+			break;
+		}
+	}
+}
+
 static void colorLine(const RegisterValue lineNumber) {
-	EditorGetString editorString;
-	editorString.StructSize = sizeof(EditorGetString);
-	editorString.StringNumber = lineNumber;
-	intptr_t editorControlResult = farAPI.EditorControl(
-		CURRENT_EDITOR,
-		ECTL_GETSTRING,
-		(intptr_t) 0,
-		&editorString
-	);
-	assert((MemoryAddress) editorControlResult == 1);
-	EXPLICITLY_UNUSED(editorControlResult);
-	
-	const wchar_t* line = editorString.StringText;
-	RegisterValue lineLength = (MemoryAddress) editorString.StringLength;
+	const wchar_t* line;
+	RegisterValue lineLength;
+	getLine(lineNumber, &line, &lineLength);
 	if (lineLength == 0) {
 		return;
 	}
@@ -51,33 +73,36 @@ static void colorLine(const RegisterValue lineNumber) {
 			break;
 		}
 	}
-	if (!containsSpaces) {
-		return;
+	
+	bool containsTabs = false;
+	for (RegisterValue pos = beginPos; pos <= endPos; ++pos) {
+		if (line[pos] == L'\t') {
+			containsTabs = true;
+			break;
+		}
 	}
 	
-	for (RegisterValue pos = beginPos; pos <= endPos; ++pos) {
-		if (line[pos] != L'\t') {
-			continue;
-		}
-
-		EditorColor editorColor;
-		editorColor.StructSize = sizeof(EditorColor);
-		editorColor.StringNumber = (intptr_t) lineNumber;
-		editorColor.ColorItem = (intptr_t) 0;
-		editorColor.StartPos = (intptr_t) pos;
-		editorColor.EndPos = (intptr_t) pos;
-		editorColor.Priority = (uintptr_t) PLUGIN_COLORING_PRIORITY;
-		editorColor.Flags = ECF_AUTODELETE;
-		editorColor.Color = PLUGIN_COLOR_USED;
-		editorColor.Owner = pluginGUID;
-		// editorControlResult =
-		farAPI.EditorControl(
-			CURRENT_EDITOR,
-			ECTL_ADDCOLOR,
-			(intptr_t) 0,
-			&editorColor
-		);
-		// assert((MemoryAddress) editorControlResult == 1);
-		// EXPLICITLY_UNUSED(editorControlResult);
+	TypeOfIndentation typeOfIndentation;
+	if (!containsSpaces) {
+		// Indentation is tabs as the indentation is not empty.
+		typeOfIndentation = TABS_INDENTATION;
+	} else if (!containsTabs) {
+		// Indentation is spaces as the indentation is not empty.
+		typeOfIndentation = SPACES_INDENTATION;
+	} else {
+		typeOfIndentation = MIXED_INDENTATION;
 	}
+	if (typeOfIndentation == MIXED_INDENTATION) {
+		colorLeadingSpaceCharacters(lineNumber);
+	} else if (
+		!isFirstLine &&
+		lastTypeOfIndentation != MIXED_INDENTATION &&
+		typeOfIndentation != lastTypeOfIndentation
+	) {
+		colorLeadingSpaceCharacters(lastLineWithIndentation);
+		colorLeadingSpaceCharacters(lineNumber);
+	}
+	lastTypeOfIndentation = typeOfIndentation;
+	isFirstLine = false;
+	lastLineWithIndentation = lineNumber;
 }
